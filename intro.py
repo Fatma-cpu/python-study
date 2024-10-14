b9ca9c1e8e5736605918e7fd809e706d9370ea24
Fatma-cpu <j86745895@gmail.com>
2024-10-14 07:27:28 -0700

    first commit

---
+++ intro.py
@@ -0,0 +1,22 @@
+# print("Fatma Shariff")
+# used to display the python output on the termminal
+fname="Fatma Shariff"
+fname="techcamp"
+print(fname)
+# write a program that adds 2 variables num1 and num2 and stores them in a variable called sum 20 and stores them in a variablecalled sum
+num1=10
+num2=20
+sum=num1+num2
+print(sum) # outpu 30
+
+# declare 2 variables fname="Fatma" lname="Shariff" add them to get a fullname
+fname="Fatma"
+lname="Shariff"
+fullname=fname+" "+lname
+print(fullname)
+
+marks=input("enter your marks ?")
+print(marks)
+# get input of phone number on the terminal
+phone=input("enter phone number?")
+print(phone)
